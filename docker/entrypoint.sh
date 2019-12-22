1401288b027c09143f942ca5aaf6c129f136ce74
Jordi Mas <jmas@softcatala.org>
2019-12-22 10:33:25 +0100

    gunicorn logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 cd /conjugador/web
-gunicorn web_search:app -b 0.0.0.0:8000
+mkdir -p /var/log/conjugador/
+gunicorn web_search:app -b 0.0.0.0:8000 --error-logfile /var/log/conjugador/gnuicorn.log
