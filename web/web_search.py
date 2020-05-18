7f2f5171fd96fe8cd4ac39dea5c2f4a0b464d490
Jordi Mas <jmas@softcatala.org>
2020-05-18 20:47:11 +0200

    Size

--- web/web_search.py
+++ web/web_search.py
@@ -38,7 +38,7 @@ def init_logging():
 
     LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
     logger = logging.getLogger()
-    hdlr = logging.handlers.RotatingFileHandler(logfile, maxBytes=1024, backupCount=1)
+    hdlr = logging.handlers.RotatingFileHandler(logfile, maxBytes=1024*1024, backupCount=1)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
