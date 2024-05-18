5c90e08cc49d5ec002a89ca4016d68e64c87a204
Jordi Mas <jmas@softcatala.org>
2024-05-18 10:23:36 +0200

    Fix

--- extractor/dictionaryfile.py
+++ extractor/dictionaryfile.py
@@ -76,7 +76,7 @@ class DictionaryFile:
         return lemmas
 
     '''
-        El diccionari d'on llegim les dades té etiquetades correctament algunes formes com a valencianes.
+        El diccionari d'on llegim les dades no té etiquetades correctament algunes formes com a valencianes.
         Esmenar-ho no es pot fer a curt plaç, ja que té implicacions en altres eines. Com a solució, marquem
         aquí de forma dinàmica aquestes formes com a valencianes.
     '''
