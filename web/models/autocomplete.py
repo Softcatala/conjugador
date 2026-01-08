2ab455e679ac63cd2f995651bca87b2fa20b4e16
NoOPeEKS <arnauapps@gmail.com>
2026-01-08 09:04:16 +0100

    style: format web files according to ruff rules

--- web/models/autocomplete.py
+++ web/models/autocomplete.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+
 class AutocompleteEntry(BaseModel):  # noqa: D101
     verb_form: str
     infinitive: str
     url: str
+
 
 type AutocompleteResponse = list[AutocompleteEntry]
