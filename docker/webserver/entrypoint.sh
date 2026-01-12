b6d552af8bac92d51282af3831515eb2b3790e06
NoOPeEKS <arnauapps@gmail.com>
2026-01-12 23:29:50 +0100

    fix(docker): select error as log level for unicorn

--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p /var/log/conjugador/
-uv run uvicorn web.main:app --host 0.0.0.0 --port 8000 2>>/var/log/conjugador/uvicorn_error.log
+uv run uvicorn web.main:app --host 0.0.0.0 --port 8000 --log-level error 2>>/var/log/conjugador/uvicorn_error.log
